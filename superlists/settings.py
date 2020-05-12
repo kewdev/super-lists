04b2e7884f744d57b23cf87eab159c925d34110b
Evgety Kazantsev <ewgeniykazancew@gmail.com>
2020-05-12 13:15:14 +0500

    Для сервера и среды разработки устанавливаются разные конфигурации

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,13 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8=v!2vkjl0$s0acekv49h^kbpr0!ef0)7s)%_m6s15__-tf519'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    SECRET_KEY = '8=v!2vkjl0$s0acekv49h^kbpr0!ef0)7s)%_m6s15__-tf519'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
